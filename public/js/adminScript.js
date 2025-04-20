f657487e506dbdeb848e627770d648a123b338bd
SonamPhuntshok <phuntshok2001@gmail.com>
2025-04-20 14:45:26 +0600

    practical 5

---
+++ public/js/adminScript.js
@@ -0,0 +1,9 @@
+// adminScript.js
+
+
+// Placeholder for future enhancements like confirmation dialogs
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Admin Dashboard loaded successfully.");
+});
+
+
